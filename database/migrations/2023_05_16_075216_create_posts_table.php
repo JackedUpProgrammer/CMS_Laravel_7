848d4000a812fc651f69f87b0e363f2d4c99a27b
Johan Scheepers <jhl.scheepers229@gmail.com>
2023-05-16 11:00:55 +0200

    added migrations for posts

--- database/migrations/2023_05_16_075216_create_posts_table.php
+++ database/migrations/2023_05_16_075216_create_posts_table.php
@@ -15,6 +15,10 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('post_image')->nullable();
+            $table->text('body');
             $table->timestamps();
         });
     }
